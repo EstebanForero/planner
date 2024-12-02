ecee50aefefa72ccb68c683cde212828910cca38
RustMaster-arch <rustmasterfullstack@gmail.com>
2024-12-02 11:05:54 -0500

    imposible bug

--- planner_service/src/lib.rs
+++ planner_service/src/lib.rs
@@ -1,7 +1,9 @@
 mod err;
 use domain::Schedule;
+use err::PlannerError;
 use general_repository::PlannerRepository;
 use uuid::Uuid;
+use tracing::error;
 
 struct PlannerService<T: PlannerRepository> {
     repository: T,
@@ -9,12 +11,13 @@ struct PlannerService<T: PlannerRepository> {
 
 impl<T: PlannerRepository> PlannerService<T> {
     pub fn new(repository: T) -> Self {
-        Self {
-            repository
-        }
+        Self { repository }
     }
 
-    pub fn create_schedule(user_id: Uuid, class_name: String, schedule: Schedule) -> err::Result<()> {
+    pub async fn create_schedule(self, user_id: Uuid, class_name: String, schedule: Schedule) -> err::Result<()> {
+        self.repository.add_schedule(user_id, class_name, schedule);
+
         Ok(())
     }
 } 
+
